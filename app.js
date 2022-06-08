c19e1eaebc76a66ece96fc9666a4aa25af42cfde
nuker <rinards.sondors@gmail.com>
2022-06-08 23:54:29 +0100

    add calculator

---
+++ app.js
@@ -0,0 +1,115 @@
+const numberButtons = document.querySelectorAll('[number]')
+const operationButtons = document.querySelectorAll('[operation]')
+const equalsButton = document.querySelector('[equals]')
+const deleteButton = document.querySelector('[delete]')
+const currentOutput= document.querySelector('[current-output]')
+const previousOutput= document.querySelector('[previous-output]')
+const keyboardInput = document.querySelector('[keyboard-input]')
+
+class Calculator {
+    constructor(previousOutput, currentOutput) {
+        this.previousOutput = previousOutput;
+        this.currentOutput = currentOutput;
+        this.delete()
+    }
+
+    delete() {
+        this.previousOperand= ''
+        this.currentOperand = ''
+        this.operation = undefined 
+    }
+
+    addNumber(number) {
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    addOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.calculate()
+          }
+        this.operation = operation 
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+    calculate() {
+        let compute
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                compute = prev + current
+                break
+            case '-':
+                compute = prev - current
+                break
+            case '*':
+                compute = prev * current
+                break
+            case '/':
+                compute = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = compute
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateField() {
+        this.currentOutput.innerText = 
+            this.getNumber(this.currentOperand)
+        if(this.operation != null) {
+            this.previousOutput.innerText = 
+            `${this.previousOperand} ${this.operation}`
+        }else { 
+            this.previousOutput.innerText = ''
+        }
+    }
+}
+
+const calculator = new Calculator(previousOutput, currentOutput)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.addNumber(button.innerText)
+        calculator.updateField()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.addOperation(button.innerText)
+        calculator.updateField()
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.calculate()
+    calculator.updateField()
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete()
+    calculator.updateField()
+})
